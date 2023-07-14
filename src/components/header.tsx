5924fad04105f568616bf6cb34a42c4af2fe2726
guilhermelima18 <guilhermelima18@hotmail.com>
2023-07-14 10:42:10 -0300

    feat: create header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { AiOutlineMenu } from "react-icons/ai";
 import logo from "@/assets/logo.svg";
+import { useState } from "react";
 
 const STATUS_AUTHENTICATED = "authenticated";
 const STATUS_UNAUTHENTICATED = "unauthenticated";
@@ -11,12 +12,21 @@ const STATUS_UNAUTHENTICATED = "unauthenticated";
 export const Header = () => {
   const { data, status } = useSession();
 
-  const onSignIn = () => {
-    signIn();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const onSignIn = () => signIn();
+
+  const onSignOut = () => {
+    setMenuIsOpen(false);
+    signOut();
+  };
+
+  const handleMenuClick = () => {
+    setMenuIsOpen(!menuIsOpen);
   };
 
   return (
-    <div className="container mx-auto p-5 flex items-center justify-between">
+    <div className="container mx-auto p-5 py-0 h-[95px] flex items-center justify-between">
       <Image src={logo} alt="Logo Travel Booking" width={32} height={32} />
 
       {status === STATUS_UNAUTHENTICATED && (
@@ -29,16 +39,30 @@ export const Header = () => {
       )}
 
       {status === STATUS_AUTHENTICATED && data.user && (
-        <div className="flex items-center gap-3">
+        <div
+          className="flex items-center gap-3 border-grayLighter border border-solid py-2 px-5 rounded-full relative"
+          onClick={handleMenuClick}
+        >
           <AiOutlineMenu size={16} />
 
           <Image
-            className="rounded-full"
+            className="rounded-full shadow-md"
             src={data.user?.image!}
             alt={data.user?.name!}
-            width={24}
-            height={24}
+            width={32}
+            height={32}
           />
+
+          {menuIsOpen && (
+            <div className="absolute top-14 left-0 w-full h-full bg-white rounded-full flex items-center justify-center shadow-md cursor-pointer">
+              <button
+                className="text-primary text-sm font-semibold"
+                onClick={onSignOut}
+              >
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
